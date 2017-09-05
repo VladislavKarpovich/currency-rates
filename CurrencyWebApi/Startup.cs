c104fb78e59b80e4d30f5cfd80d31346852ecf10
Vladislav <vladislavkarpovich1@gmail.com>
2017-09-05 10:51:36 +0300

    Hangfire added

--- CurrencyWebApi/Startup.cs
+++ CurrencyWebApi/Startup.cs
@@ -1,4 +1,5 @@
-﻿using ExadelPractice;
+﻿using Hangfire;
+using ExadelPractice;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
@@ -9,11 +10,17 @@ namespace CurrencyWebApi
 {
     public class Startup
     {
+        const string conncectionString = @"data source=(localdb)\MSSQLLocalDB;initial catalog=CurrencyDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;";
 
         public void Configuration(IAppBuilder app)
         {
             var config = new HttpConfiguration();
             var kernel = NinjectConfig.CreateKernel();
+
+            Hangfire.GlobalConfiguration.Configuration.UseSqlServerStorage(conncectionString);
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
 
             NinjectConfig.Register(app, config, kernel);
             WebApiConfig.Register(config);
